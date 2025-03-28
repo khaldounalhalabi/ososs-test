58a17d1b2fb2fdce33c2fa2fc9eac6f1e15b6cdd
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2025-03-28 15:46:01 +0300

    Auth feature: add Role seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            RoleSeeder::class,
             CountrySeeder::class,
             UserSeeder::class,
         ]);
