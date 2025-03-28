33cb3526d447692d823baf482ff6edabcf9e6c75
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2025-03-28 15:54:56 +0300

    Auth feature: adding localization files

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use App\RoleEnum;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 use Spatie\Permission\Middleware\RoleMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
---
+++ lang/en/site.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'success' => 'Success',
+    'there_is_no_data' => 'There is no data',
+    'get_successfully' => 'Data retrieved successfully',
+    'stored_successfully' => 'Data stored successfully',
+    'update_successfully' => 'Data updated successfully',
+    'delete_successfully' => 'Data deleted successfully',
+    'failed' => 'There is been an error during the operation',
+    'registered_successfully' => 'Your account has been registered successfully',
+    'invalid_credentials' => 'Invalid credentials',
+    'logged_in_successfully' => 'You logged in successfully',
+    'logged_out_successfully' => 'You logged out successfully',
+    'unauthorized' => 'You are not authorized to do this action',
+    'reset_password_code_sent' => 'A reset password code was sent to your email',
+    'refresh_token_successfully' => 'Your refresh token was successfully updated',
+    'invalid_email' => 'Invalid email address',
+    'invalid_code' => 'Invalid code',
+    'password_changed' => 'Your password has been changed successfully',
+    'updated_successfully' => 'Your account has been updated successfully',
+    'unknown_error' => 'Unknown error',
+];
